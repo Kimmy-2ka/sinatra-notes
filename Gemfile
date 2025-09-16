aa28631c50d0d55015653cd38c54d4319e42e6b7
Kimmy-2ka <kimmy2ka.ka1ros3@gmail.com>
2025-09-16 12:09:13 -0700

    メモアプリを新規実装した

---
+++ Gemfile
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+group :development do
+  gem 'erb_lint', require: false
+  gem 'rubocop-fjord', require: false
+end
+
+gem 'sinatra', '~> 4.1'
+
+gem 'webrick', '~> 1.9'
+
+gem 'puma', '~> 7.0'
+
+gem 'rackup', '~> 2.2'
+
+gem 'sinatra-contrib', '~> 4.1'
---
+++ notes.rb
@@ -0,0 +1,105 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader'
+
+# Description of Note class
+# JSONファイルの読込・保存・追加・編集・削除する機能を有する。
+class Note
+  def self.load
+    JSON.parse(File.read('notes.json'))
+  end
+
+  def self.save(notes)
+    File.open('notes.json', 'w') do |file|
+      JSON.dump(notes, file)
+    end
+  end
+
+  def self.create(note_title, content)
+    notes = load
+    notes << { id: notes.size, note_title: note_title, content: content, delete: false }
+    save(notes)
+  end
+
+  def self.find(id)
+    load.find { |note| note['id'] == id }
+  end
+
+  def self.edit(note_title, content, id)
+    notes =
+      load.map do |note|
+        if note['id'] == id
+          note.merge('note_title' => note_title, 'content' => content)
+        else
+          note
+        end
+      end
+    save(notes)
+  end
+
+  def self.delete(id)
+    notes =
+      load.map do |note|
+        note['id'] == id ? note.merge('delete' => true) : note
+      end
+    save(notes)
+  end
+end
+
+helpers do
+  def escape(text)
+    Rack::Utils.escape_html(text)
+  end
+end
+
+get '/' do
+  redirect '/notes'
+end
+
+not_found do
+  @title = 'Entry is not found'
+  erb :not_found
+end
+
+get '/notes' do
+  @title = 'メモアプリ'
+  @notes = Note.load
+  erb :index
+end
+
+get '/notes/new' do
+  @title = '新規作成'
+  erb :new
+end
+
+post '/notes' do
+  Note.create(params[:note_title], params[:content])
+  redirect '/notes'
+end
+
+get '/notes/:id' do
+  @note = Note.find(params[:id].to_i)
+  if @note && !@note['delete']
+    @title = @note['note_title']
+    erb :detail
+  else
+    halt 404
+  end
+end
+
+get '/notes/:id/edit' do
+  @note = Note.find(params[:id].to_i)
+  @title = "編集 #{@note['note_title']}"
+  erb :edit
+end
+
+patch '/notes/:id' do
+  Note.edit(params[:note_title], params[:content], params[:id].to_i)
+  redirect '/notes'
+end
+
+delete '/notes/:id' do
+  Note.delete(params[:id].to_i)
+  redirect '/notes'
+end
